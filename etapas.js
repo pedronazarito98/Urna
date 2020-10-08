5e79ce905c08aef38807ded0ddfc90430cf1aeea
pedronazarito98 <pedronazarito@gmail.com>
2020-10-08 13:04:24 -0300

    Versão Final

---
+++ etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos:[
+            {
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url: 'adultoH.png', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva ',
+                partido: 'DEFG',
+                fotos: [
+                    {url: 'JovemH.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos:[
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cis',
+                fotos:[
+                    {url: 'adultaM.png', legenda: 'Prefeitos'},
+                    {url: 'IdosoM.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos:[
+                    { url: 'JovemM.png', legenda: ' Prefeito'},
+                    { url: 'IdosoM.png', legenda: 'Vice-Prefeito', small: true }
+                ]
+            }
+        ]
+    }
+];
+
+// 52:11
